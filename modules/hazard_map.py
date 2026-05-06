75ba6f73b1da794205c5376dfcc4ccdddd8cbfa6
Felicia123-prog <barchafel@gmail.com>
2026-05-06 12:26:03 -0300

    Update hazard_map.py

--- modules/hazard_map.py
+++ modules/hazard_map.py
@@ -1,4 +1,5 @@
 import folium
+from folium.plugins import HeatMap
 import geopandas as gpd
 import numpy as np
 from modules.utils import ensure_shapefile_unzipped
@@ -12,24 +13,31 @@ def create_hazard_map(raster, lons, lats,
 
     shapefile_path = f"{shapefile_folder}/Distrikten_AdjAOI.shp"
 
+    # Shapefile inladen
     gdf = gpd.read_file(shapefile_path)
-    center = [gdf.geometry.centroid.y.mean(), gdf.geometry.centroid.x.mean()]
+
+    # Kaart centreren op Suriname
+    center = [
+        gdf.geometry.centroid.y.mean(),
+        gdf.geometry.centroid.x.mean()
+    ]
 
     m = folium.Map(location=center, zoom_start=7, tiles="cartodbpositron")
 
-    # Raster naar punten
+    # Raster omzetten naar HeatMap punten
     points = []
     for i in range(len(lats)):
         for j in range(len(lons)):
-            points.append([lats[i], lons[j], raster[i, j]])
+            points.append([lats[i], lons[j], float(raster[i, j])])
 
-    folium.plugins.HeatMap(
-        [[p[0], p[1], p[2]] for p in points],
+    HeatMap(
+        points,
         radius=18,
         blur=25,
         max_zoom=7
     ).add_to(m)
 
+    # Districten toevoegen
     folium.GeoJson(gdf).add_to(m)
 
     return m
